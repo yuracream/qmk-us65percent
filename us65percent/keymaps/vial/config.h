7d9abcf0b6a2d568503c0fcd3efdc0e3029d4ab0
yuracream <96818771+yuracream@users.noreply.github.com>
2025-10-28 10:40:10 +0900

    Add files via upload

---
+++ us65percent/keymaps/vial/config.h
@@ -0,0 +1,4 @@
+#pragma once
+#define VIAL_KEYBOARD_UID {0xC3, 0x0A, 0x6F, 0x29, 0xC5, 0x45, 0xBE, 0xB7}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
---
+++ us65percent/keymaps/vial/rules.mk
@@ -0,0 +1,5 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+# 初期編集のため安全機能は環境変数で緩める（ビルド時に -e VIAL_INSECURE=yes を付ける）
+CONSOLE_ENABLE = yes
+CONSOLE_ENABLE = yes
