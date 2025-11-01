acc6b884ffe7bcc34d5db24fada8274dbcae928b
yuracream <96818771+yuracream@users.noreply.github.com>
2025-11-01 14:34:32 +0900

    Update keymap.c

--- us65percent/keymaps/vial/keymap.c
+++ us65percent/keymaps/vial/keymap.c
@@ -12,17 +12,29 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_65_ansi(
-  KC_ESCAPE, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, S(KC_6), KC_BACKSLASH,
-  KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_DELETE,
-  KC_CAPS_LOCK, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SEMICOLON, KC_QUOTE, KC_ENTER,
-  KC_LEFT_SHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_BACKSLASH, KC_RIGHT_SHIFT,
-  KC_LEFT_GUI, KC_LEFT_CTRL, KC_LEFT_ALT, KC_NO, KC_SPACE, KC_NO, KC_NO, KC_NO, KC_NO, KC_RIGHT_ALT, KC_NO, KC_RIGHT_GUI
-),
+              KC_ESCAPE,            KC_1,            KC_2,            KC_3,
+                   KC_4,            KC_5,            KC_6,            KC_7,
+                   KC_8,            KC_9,            KC_0,        KC_MINUS,
+               KC_EQUAL,    KC_BACKSLASH,        KC_GRAVE,          KC_TAB,
+                   KC_Q,            KC_W,            KC_E,            KC_R,
+                   KC_T,            KC_Y,            KC_U,            KC_I,
+                   KC_O,            KC_P, KC_LEFT_BRACKET, KC_RIGHT_BRACKET,
+           KC_BACKSPACE,    KC_CAPS_LOCK,            KC_A,            KC_S,
+                   KC_D,            KC_F,            KC_G,            KC_H,
+                   KC_J,            KC_K,            KC_L,    KC_SEMICOLON,
+               KC_QUOTE,        KC_ENTER,   KC_LEFT_SHIFT,            KC_Z,
+                   KC_X,            KC_C,            KC_V,            KC_B,
+                   KC_N,            KC_M,        KC_COMMA,          KC_DOT,
+               KC_SLASH,    KC_BACKSLASH,  KC_RIGHT_SHIFT,     KC_LEFT_GUI,
+           KC_LEFT_CTRL,     KC_LEFT_ALT,   KC_LANGUAGE_2,        KC_SPACE,
+             KC_MS_BTN1,      KC_MS_BTN2,   KC_LANGUAGE_1,  KC_TRANSPARENT,
+           KC_RIGHT_ALT,  KC_APPLICATION,    KC_RIGHT_GUI
+    ),
     [1] = LAYOUT_65_ansi(
-         KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,
-         KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,
-         KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,
-         KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,
+         KC_TRANSPARENT,           KC_F1,           KC_F2,           KC_F3,
+                  KC_F4,           KC_F5,           KC_F6,           KC_F7,
+                  KC_F8,           KC_F9,          KC_F10,          KC_F11,
+                 KC_F12,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,
          KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,
          KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,
          KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,  KC_TRANSPARENT,
@@ -89,10 +101,6 @@ const vial_tap_dance_entry_t default_tap_dance_entries[] = {
     TAP_DANCE_ENTRY(KC_NO, KC_NO, KC_NO, KC_NO, 200),
     TAP_DANCE_ENTRY(KC_NO, KC_NO, KC_NO, KC_NO, 200),
     TAP_DANCE_ENTRY(KC_NO, KC_NO, KC_NO, KC_NO, 200),
-    TAP_DANCE_ENTRY(KC_NO, KC_NO, KC_NO, KC_NO, 200),
-    TAP_DANCE_ENTRY(KC_NO, KC_NO, KC_NO, KC_NO, 200),
-    TAP_DANCE_ENTRY(KC_NO, KC_NO, KC_NO, KC_NO, 200),
-    TAP_DANCE_ENTRY(KC_NO, KC_NO, KC_NO, KC_NO, 200),
     TAP_DANCE_ENTRY(KC_NO, KC_NO, KC_NO, KC_NO, 200)
 };
 #endif
@@ -101,10 +109,6 @@ const vial_tap_dance_entry_t default_tap_dance_entries[] = {
 #define COMBO_ENTRY(k1, k2, k3, k4, result) ((vial_combo_entry_t){.input ={k1, k2, k3, k4}, .output = result})
 #if VIAL_COMBO_ENTRIES > 0
 const vial_combo_entry_t PROGMEM default_combo_entries[] = {
-    COMBO_ENTRY(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-    COMBO_ENTRY(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-    COMBO_ENTRY(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-    COMBO_ENTRY(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
     COMBO_ENTRY(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
     COMBO_ENTRY(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
     COMBO_ENTRY(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
@@ -215,18 +219,3 @@ void __wrap_dynamic_keymap_reset(void) {
 
 
 /* USER CODE BEGIN *//* USER CODE END */
-#include "debug.h"
-void keyboard_post_init_user(void) {
-    debug_enable = true;
-    debug_matrix = true;
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {
-        uprintf("press r%u c%u kc %u\n",
-                record->event.key.row,
-                record->event.key.col,
-                keycode);
-    }
-    return true;
-}
