74d46b0d1a7ebabe340b298cd7abf78c3057921d
yuracream <96818771+yuracream@users.noreply.github.com>
2025-11-03 21:29:13 +0900

    Update config.h

--- us65percent/config.h
+++ us65percent/config.h
@@ -1,12 +1,1 @@
 #pragma once
-
-// ★必須：Vialのキーボード固有ID（8バイト）
-#define VIAL_KEYBOARD_UID {0xD1, 0x65, 0x65, 0x25, 0xFE, 0xED, 0x00, 0x01}
-
-// ★推奨：アンロック用の2キー（実在キー座標に合わせてOK）
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
-
-// （任意）レイヤ数を明示したい場合
-// #define DYNAMIC_KEYMAP_LAYER_COUNT 8
-
